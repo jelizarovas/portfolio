f5f69b0f2b0478d3279b07b38b40ad4da4ba447f
Arnas Jelizarovas <jelizarovas@gmail.com>
2022-11-01 01:41:07 +0200

    added router and tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ function App() {
   return (
     <div className="App">
       <h1>Portfolio of Arnas Jelizarovas</h1>
+
+      <ul className="bg-red-500">
+        <li>Seimossaknis.lt</li>
+      </ul>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import "./index.css";
+import ErrorPage from "./error-page";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      errorElement: <ErrorPage />,
+    },
+  ],
+  { basename: "/portfolio/" }
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
